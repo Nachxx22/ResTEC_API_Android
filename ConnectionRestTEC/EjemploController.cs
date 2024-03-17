00612fa5f4a125484950e5b0bbbcb7919f3eb66e
Nachxx22 <nachxx22@gmail.com>
2024-03-17 09:37:54 -0600

    Change API, send JSON, list of platillos

--- ConnectionRestTEC/EjemploController.cs
+++ ConnectionRestTEC/EjemploController.cs
@@ -1,18 +1,54 @@
-namespace ConnectionRestTEC;
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
-
-[ApiController]
-
-[Route("api")]
-public class EjemploController : ControllerBase
+namespace ConnectionRestTEC
 {
-    
-    
-    [HttpGet]
-    [Route("ejemplo2")]
-    public ActionResult<string> Get()
+    [ApiController]
+    [Route("api")]
+    public class EjemploController : ControllerBase
     {
-        return "Hola desde la API REST en C#";
+        [HttpGet]
+        [Route("ejemplo2")]
+        public ActionResult<IEnumerable<Platillo>> Get()
+        {
+            // Crear una lista de platillos con datos de ejemplo
+            List<Platillo> platillos = new List<Platillo>
+            {
+                new Platillo
+                {
+                    Nombre = "Platillo 1",
+                    Precio = 10.99m,
+                    Tipo = "Entrada",
+                    TiempoEstimado = TimeSpan.FromMinutes(20),
+                    Descripcion = "Este es el platillo 1",
+                    Calorias = 300
+                },
+                new Platillo
+                {
+                    Nombre = "Platillo 2",
+                    Precio = 15.99m,
+                    Tipo = "Plato Principal",
+                    TiempoEstimado = TimeSpan.FromMinutes(30),
+                    Descripcion = "Este es el platillo 2",
+                    Calorias = 500
+                }
+            };
+
+            // Devolver la lista de platillos como JSON
+            return Ok(platillos);
+        }
+    }
+
+    public class Platillo
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+        public string Tipo { get; set; }
+        public TimeSpan TiempoEstimado { get; set; }
+        public string Descripcion { get; set; }
+        public int Calorias { get; set; }
     }
 }
+
+
